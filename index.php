c96e13945a045aa65afffecc5cfb393b7384aec7
DontitoSama <jokonito06@gmail.com>
2024-01-15 15:29:24 +0100

    adding js file and bootstrap file

--- index.php
+++ index.php
@@ -9,7 +9,9 @@ $read->execute();
 <html>
     <head>
         <?php include('partials/head.php'); ?>
+        <link rel="stylesheet" type="text/css" href="public/css/bootstrap.css"/>
         <link rel="stylesheet" type="text/css" href="public/css/styles.css"/>
+        <script type="text/javascript" src="public/js/index.js"></script>
         <title>Accueil</title>
     </head>
     <body>
@@ -39,5 +41,31 @@ $read->execute();
         </table>
 
         <button id="createProduct">Ajouter un produit</button>
+
+        <div class="modal draggable" id="createModal">
+        <div class="modal-content">
+            <span class="close" id="closeModal">&times;</span>
+            <h2>Ajouter un produit</h2>
+                <div class="form-group">
+                <label for="model">Modèle :</label>
+                <input type="text" id="model" name="model"></label>
+
+                <label for="color">Couleur :</label>
+                <input type="text" id="color" name="color"></label>
+
+                <label for="capacity">Capacité (en giga) :</label>
+                <input type="number" id="capacity" name="capacity"></label>
+
+                <label for="release_year">Année de sortie :</label>
+                <input type="date" id="release_year" name="release_year"></label>
+                </div>
+
+                <div class="form-group">
+                    <button id="validProduct" class="btn-save">Valider</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
     </body>
 </html>
---
+++ public/js/index.js
@@ -0,0 +1,92 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    const modal = document.getElementById("createModal");
+    const createProduct = document.getElementById("createProduct");
+    const closeModal = document.getElementById("closeModal");
+    const validProduct = document.getElementById("validProduct");
+
+    createProduct.addEventListener("click", () => {
+        modal.style.display = "block"; 
+    });
+
+    let isDragging = false;
+    let offset = { x: 0, y: 0 };
+    
+    document.addEventListener("mouseleave", () => {
+        if (isDragging) {
+            isDragging = false;
+            modal.style.cursor = "grab";
+        }
+    });
+    
+    modal.addEventListener("mousedown", (e) => {
+        isDragging = true;
+        offset.x = e.clientX - modal.getBoundingClientRect().left;
+        offset.y = e.clientY - modal.getBoundingClientRect().top;
+        modal.style.cursor = "grabbing";
+    });
+    
+    document.addEventListener("mousemove", (e) => {
+        if (!isDragging) return;
+        modal.style.left = e.clientX - offset.x + "px";
+        modal.style.top = e.clientY - offset.y + "px";
+    });
+    
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+        modal.style.cursor = "grab";
+    });
+    
+    validProduct.addEventListener("click", (event) => {
+        if (!validateForm()) {
+            event.preventDefault();
+        } else {
+            modal.style.display = "none"; 
+        }
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.style.display = "none"; 
+    });
+
+    function validateForm() {
+        var valid = true;
+
+        const model = document.getElementById("model");
+        if (model.value.trim() === '' || model.value.length === 0 || model.value.length > 30
+        ) {
+            alert("Veuillez écrire un nom entre 1 et 30 caractères.");
+            valid = false;
+        }
+
+        const color = document.getElementById("color");
+        if (color.value.trim() === '' || color.value.length === 0 || color.value.length > 30
+        ) {
+            alert("Veuillez écrire une couleur entre 1 et 30 caractères.");
+            valid = false;
+        }
+
+        function isValidDateTime(dateTimeString) {
+            // Format de date et heure local (ISO 8601)
+            const dateTimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
+        
+            // Vérifier si la chaîne de date correspond au format
+            return dateTimeRegex.test(dateTimeString);
+        }
+
+        const date = document.getElementById("release_year");
+        if (!isValidDateTime(date.value)) {
+            alert("Veuillez entrer une date valide.");
+            valid = false;
+        }
+
+        return valid; 
+    }
+
+    document.getElementById("capacity").addEventListener("input", function () {
+        const capacity = this.value;
+        if (capacity < 0 || capacity > 1000) {
+            alert("Veuillez entrer une valeur 0 et 1000.");
+            this.value = "";
+        }
+    });
+});
