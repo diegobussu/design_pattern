2be52f36841e23f37f48f4919b433f2f9e4cdc8c
DontitoSama <jokonito06@gmail.com>
2024-01-15 11:52:05 +0100

    lol

---
+++ factory.php
@@ -0,0 +1,39 @@
+<?php
+
+// Interface représentant le produit que la fabrique va créer
+interface Product
+{
+    public function getName(): string;
+}
+
+// Implémentation concrète de l'interface Product
+class ConcreteProduct implements Product
+{
+    public function getName(): string
+    {
+        return "Concrete Product";
+    }
+}
+
+// Interface de la fabrique
+interface Factory
+{
+    public function createProduct(): Product;
+}
+
+// Implémentation concrète de la fabrique
+class ConcreteFactory implements Factory
+{
+    public function createProduct(): Product
+    {
+        return new ConcreteProduct();
+    }
+}
+
+// Utilisation de la fabrique
+$factory = new ConcreteFactory();
+$product = $factory->createProduct();
+
+echo $product->getName(); // Affiche "Concrete Product"
+
+?>
