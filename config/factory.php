d856650080ea4219b4b1bffb3e64366f72d4e5de
DontitoSama <jokonito06@gmail.com>
2024-01-15 16:26:43 +0100

    update factory.php

--- config/factory.php
+++ config/factory.php
@@ -5,16 +5,23 @@ interface Product
 {
     public function getName(): string;
     public function getReleaseYear(): string;
+    public function getColor(): string;
+    public function getCapacity(): int;
 }
 
 // Class for Iphone product
 class Iphone implements Product
 {
     private $model;
+    private $color;
+    private $capacity;
     private $release_year;
 
-    public function __construct ($model, $release_year) {
+    public function __construct($model, $color, $capacity, $release_year)
+    {
         $this->model = $model;
+        $this->color = $color;
+        $this->capacity = $capacity;
         $this->release_year = $release_year;
     }
 
@@ -27,16 +34,31 @@ class Iphone implements Product
     {
         return "Année de sortie " . $this->release_year;
     }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getCapacity(): int
+    {
+        return $this->capacity;
+    }
 }
 
-// Class for Iphone product
+// Class for Ipad product
 class Ipad implements Product
 {
     private $model;
+    private $color;
+    private $capacity;
     private $release_year;
 
-    public function __construct ($model, $release_year) {
+    public function __construct($model, $color, $capacity, $release_year)
+    {
         $this->model = $model;
+        $this->color = $color;
+        $this->capacity = $capacity;
         $this->release_year = $release_year;
     }
 
@@ -49,43 +71,81 @@ class Ipad implements Product
     {
         return "Année de sortie " . $this->release_year;
     }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getCapacity(): int
+    {
+        return $this->capacity;
+    }
 }
 
 // Interface du stock Apple
 interface Apple
 {
-    public function InfosProduct(): Product;
+    public function addProduct(string $model, string $color, int $capacity, string $releaseYear): void;
+    public function getProduct(string $productName): ?Product;
+    public function updateProduct(string $productName, Product $updatedProduct): void;
+    public function deleteProduct(string $productName): void;
 }
 
 class iPhoneStock implements Apple
 {
-    private $model;
-    private $release_year;
+    private $products = [];
 
-    public function __construct ($model, $release_year) {
-        $this->model = $model;
-        $this->release_year = $release_year;
+    public function addProduct(string $model, string $color, int $capacity, string $releaseYear): void
+    {
+        $product = new Iphone($model, $color, $capacity, $releaseYear);
+        $this->products[$product->getName()] = $product;
     }
 
-    public function InfosProduct(): Product
+    public function getProduct(string $productName): ?Product
     {
-        return new Iphone($this->model, $this->release_year);
+        return $this->products[$productName] ?? null;
+    }
+
+    public function updateProduct(string $productName, Product $updatedProduct): void
+    {
+        if (isset($this->products[$productName])) {
+            $this->products[$productName] = $updatedProduct;
+        }
+    }
+
+    public function deleteProduct(string $productName): void
+    {
+        unset($this->products[$productName]);
     }
 }
 
 class iPadStock implements Apple
 {
-    private $model;
-    private $release_year;
+    private $products = [];
 
-    public function __construct ($model, $release_year) {
-        $this->model = $model;
-        $this->release_year = $release_year;
+    public function addProduct(string $model, string $color, int $capacity, string $releaseYear): void
+    {
+        $product = new Ipad($model, $color, $capacity, $releaseYear);
+        $this->products[$product->getName()] = $product;
     }
 
-    public function InfosProduct(): Product
+    public function getProduct(string $productName): ?Product
     {
-        return new Ipad($this->model, $this->release_year);
+        return $this->products[$productName] ?? null;
+    }
+
+    public function updateProduct(string $productName, Product $updatedProduct): void
+    {
+        if (isset($this->products[$productName])) {
+            $this->products[$productName] = $updatedProduct;
+        }
+    }
+
+    public function deleteProduct(string $productName): void
+    {
+        unset($this->products[$productName]);
     }
 }
+
 ?>
